f9f76a313d47fd7ddbf8916ea1deb67dd99fe377
Michal Margiel <Michal.Margiel@gmail.com>
2017-04-02 18:17:12 +0200

    remove unnecessary modules from appmodule

--- confitura-page/src/app/app.module.ts
+++ confitura-page/src/app/app.module.ts
@@ -3,13 +3,12 @@ import {routing} from "./app.routing";
 import {NgModule} from "@angular/core";
 import {LayoutModule} from "./layout/layout.module";
 import {PagesModule} from "./pages/pages.module";
-import {SharedModule} from "./shared/shared.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ProfileModule} from "./profile/profile.module";
 import {UserService} from "./pages/profile/user.service";
 @NgModule({
-    imports: [LayoutModule, routing, PagesModule, SharedModule, BrowserModule, BrowserAnimationsModule, ProfileModule],
+    imports: [LayoutModule, PagesModule, BrowserModule, BrowserAnimationsModule, ProfileModule, routing],
     providers: [UserService],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
