e13bb1f5067a2451bb2c88a39ea3ae4e9a90a470
Michal Margiel <Michal.Margiel@gmail.com>
2017-04-13 20:45:35 +0200

    enable cors for data rest endpoints

--- jelatyna-backend/src/main/java/pl/confitura/jelatyna/infrastructure/CorsConfiguration.java
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/infrastructure/CorsConfiguration.java
@@ -1,27 +1,23 @@
 package pl.confitura.jelatyna.infrastructure;
 
-import static org.springframework.web.bind.annotation.RequestMethod.*;
-
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//@Configuration
+@Configuration
 //@Profile(DEV)
 public class CorsConfiguration {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/**");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//
+//                registry.addMapping("/**");
+//            }
+//        };
+//    }
 
     @Bean
     public RepositoryRestConfigurerAdapter repositoryRestConfigurerAdapter() {
@@ -30,8 +26,7 @@ public class CorsConfiguration {
             public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
                 config
                         .getCorsRegistry()
-                        .addMapping("/**")
-                        .allowedMethods(DELETE.name(), GET.name(), POST.name(), OPTIONS.name());
+                        .addMapping("/**");
             }
         };
     }
