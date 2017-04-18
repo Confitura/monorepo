6fbaf0c66d026442c838f0bf99af8a6816343908
Michal Margiel <Michal.Margiel@gmail.com>
2017-04-18 19:43:24 +0200

    implement IsAuthenticatedGuard

--- confitura-page/src/app/profile/profile.module.ts
+++ confitura-page/src/app/profile/profile.module.ts
@@ -7,11 +7,12 @@ import {PersonModule} from "../persons/persons.module";
 import {PresentationEditComponent} from "./presentation-edit/presentation-edit.component";
 import {TagInputModule} from "ng2-tag-input";
 import {ProfileCompleteGuard} from "./shared/profile-complete-guard.service";
+import {IsAuthenticatedGuard} from "./shared/is-authenticated-guard.service";
 
 @NgModule({
     imports: [routing, SharedModule, PersonModule,TagInputModule],
     declarations: [ProfileEditComponent, ProfileViewComponent, PresentationEditComponent],
-    providers: [ProfileCompleteGuard]
+    providers: [ProfileCompleteGuard, IsAuthenticatedGuard]
 
 })
 export class ProfileModule {
--- confitura-page/src/app/profile/profile.routing.ts
+++ confitura-page/src/app/profile/profile.routing.ts
@@ -4,13 +4,13 @@ import {ProfileEditComponent} from "./profile-edit/profile-edit.component";
 import {ProfileViewComponent} from "./profile-view/profile-view.component";
 import {PresentationEditComponent} from "./presentation-edit/presentation-edit.component";
 import {ProfileCompleteGuard} from "./shared/profile-complete-guard.service";
+import {IsAuthenticatedGuard} from "./shared/is-authenticated-guard.service";
 const appRoutes: Routes = [
-    {path: 'profile', component: ProfileViewComponent},
-    {path: 'profile/edit', component: ProfileEditComponent, canDeactivate: [ProfileCompleteGuard]},
-    {path: 'profile/:id/edit', component: ProfileEditComponent, canDeactivate: [ProfileCompleteGuard]},
-    {path: 'profile/:id', component: ProfileViewComponent},
-    {path: 'presentation', component: PresentationEditComponent},
-    {path: 'presentation/:id', component: PresentationEditComponent},
-
+    {path: 'profile', component: ProfileViewComponent, canActivate: [IsAuthenticatedGuard]},
+    {path: 'profile/edit', component: ProfileEditComponent, canDeactivate: [ProfileCompleteGuard], canActivate: [IsAuthenticatedGuard]},
+    {path: 'profile/:id/edit', component: ProfileEditComponent, canDeactivate: [ProfileCompleteGuard], canActivate: [IsAuthenticatedGuard]},
+    {path: 'profile/:id', component: ProfileViewComponent, canActivate: [IsAuthenticatedGuard]},
+    {path: 'presentation', component: PresentationEditComponent, canActivate: [IsAuthenticatedGuard]},
+    {path: 'presentation/:id', component: PresentationEditComponent, canActivate: [IsAuthenticatedGuard]}
 ];
 export const routing: ModuleWithProviders = RouterModule.forChild(appRoutes);
