ce5b457e947dbbaa55318aeeb3a333d8c057830f
Michal Margiel <Michal.Margiel@gmail.com>
2017-04-27 22:46:15 +0200

    partners banner on homepage

--- confitura-page/src/app/pages/pages.module.ts
+++ confitura-page/src/app/pages/pages.module.ts
@@ -15,9 +15,10 @@ import {PartnerComponent} from "./partners/partner/partner.component";
 import {RouterModule} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {FileUploadModule} from "ng2-file-upload";
+import {PartnerBannerComponent} from "./partners/partner-banner/partner-banner.component";
 @NgModule({
     imports: [SharedModule, PersonModule, RouterModule, FileUploadModule],
-    declarations: [HomeComponent, NewsBannerComponent, NewsComponent, AboutComponent, PageComponent, PartnersComponent, PartnerComponent, LoginComponent],
+    declarations: [HomeComponent, NewsBannerComponent, NewsComponent, AboutComponent, PageComponent, PartnersComponent, PartnerComponent, LoginComponent, PartnerBannerComponent],
     providers: [NewsService, OrganizerService, PageService],
     exports: [HomeComponent, AboutComponent]
 })
