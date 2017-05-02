fbe61d786200627c19b7ce5efe7cc1170150a380
Michal Margiel <Michal.Margiel@gmail.com>
2017-05-02 21:59:52 +0200

    ignore tests

--- jelatyna-backend/src/test/java/pl/confitura/jelatyna/user/UserControllerTest.java
+++ jelatyna-backend/src/test/java/pl/confitura/jelatyna/user/UserControllerTest.java
@@ -1,12 +1,14 @@
 package pl.confitura.jelatyna.user;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserControllerTest {
 
     @Test
+    @Ignore
     @DisplayName("should test")
     void should_test() {
         Assertions.assertThat(true).isTrue();
