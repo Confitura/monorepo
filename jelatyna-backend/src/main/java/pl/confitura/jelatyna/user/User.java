96ac12519a2b48b7d6455de282395b77d6cf1063
Michal Margiel <Michal.Margiel@gmail.com>
2017-05-17 22:40:20 +0200

    projection for getting user with presentations

--- jelatyna-backend/src/main/java/pl/confitura/jelatyna/user/User.java
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/user/User.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.springframework.data.rest.core.config.Projection;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -39,5 +41,30 @@ public class User {
 
     interface Edit {
 
+    }
+
+    @Projection(name = "withPresentations", types = { User.class })
+    interface WithPresentations {
+        String getId();
+
+        String getOrigin();
+
+        String getName();
+
+        String getEmail();
+
+        String getBio();
+
+        String getTwitter();
+
+        String getGithub();
+
+        String getWww();
+
+        String getPhoto();
+
+        boolean isAdmin();
+
+        Set<Presentation> getPresentations();
     }
 }
