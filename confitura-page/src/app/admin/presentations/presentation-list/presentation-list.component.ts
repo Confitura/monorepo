83dda11e5593fd7f124ff655716e4964445b5f6e
Michal Margiel <Michal.Margiel@gmail.com>
2017-05-18 23:36:48 +0200

    scroll to selected presentation

--- confitura-page/src/app/admin/presentations/presentation-list/presentation-list.component.ts
+++ confitura-page/src/app/admin/presentations/presentation-list/presentation-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from "@angular/core";
+import {AfterViewChecked, Component, OnInit} from "@angular/core";
 import {PresentationService} from "../../../profile/shared/presentation.service";
 import {Observable} from "rxjs/Observable";
 import {Presentation} from "../../../profile/shared/presentation.model";
@@ -6,22 +6,43 @@ import "./presentation-list.component.scss";
 import {PersonModalService} from "../../../persons/person-modal/person-modal.service";
 import {User} from "../../../pages/profile/user.model";
 import {UserService} from "../../../pages/profile/user.service";
+import {ActivatedRoute} from "@angular/router";
 @Component({
     templateUrl: "./presentation-list.component.html"
 })
-export class PresentationListComponent implements OnInit {
+export class PresentationListComponent implements OnInit, AfterViewChecked {
+
+
     list: Observable<Presentation[]>;
 
     ngOnInit(): void {
         this.list = this.service.getAll();
+
     }
 
-    constructor(private service: PresentationService, private personModalService: PersonModalService, private userService: UserService) {
+    ngAfterViewChecked(): void {
+        let presentationId = this.route.snapshot.fragment;
+        if (presentationId) {
+            const element = document.getElementById(presentationId);
+            if (element) {
+                console.log(element.offsetTop);
+                window.scrollTo(0, element.offsetTop - 100);
+            }
+
+        }
+    }
+
+
+    constructor(private service: PresentationService, private personModalService: PersonModalService, private userService: UserService, private route: ActivatedRoute) {
     }
 
     show(speaker: User) {
         this.userService.getBy(speaker.id, "withPresentations")
             .subscribe(user => this.personModalService.showFor(user));
+    }
+
+    clean(id: string) {
+        return id.replace(/-/g, "")
     }
 
 
--- confitura-page/src/app/persons/person-modal/person-modal.component.ts
+++ confitura-page/src/app/persons/person-modal/person-modal.component.ts
@@ -2,6 +2,8 @@ import {Component} from "@angular/core";
 import {PersonModalService} from "./person-modal.service";
 import "./person-modal.component.scss";
 import {User} from "../../pages/profile/user.model";
+import {Presentation} from "../../profile/shared/presentation.model";
+import {Router} from "@angular/router";
 @Component({
     selector: "jl-person-modal",
     templateUrl: "./person-modal.component.html"
@@ -9,16 +11,21 @@ import {User} from "../../pages/profile/user.model";
 export class PersonModalComponent {
     model: User;
 
-    constructor(service: PersonModalService) {
+    constructor(service: PersonModalService, private router: Router) {
         service.changed.subscribe((user: User) => {
             this.model = user;
             this.openModal();
-
         });
+    }
+
+    goTo(presentation: Presentation) {
+        $(".modal").modal("hide");
+        this.router.navigate(["/admin2/presentations"], {fragment: presentation.id})
     }
 
     private openModal() {
         $(".modal").modal({show: true});
     }
+
 
 }
