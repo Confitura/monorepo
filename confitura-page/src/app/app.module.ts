f515482240379998c62f3e1741d6bb66b40c082b
Michal Margiel <Michal.Margiel@gmail.com>
2018-01-12 06:33:29 +0100

    add service worker

--- confitura-page/src/app/app.module.ts
+++ confitura-page/src/app/app.module.ts
@@ -9,12 +9,21 @@ import {NgLoadingBarModule} from 'ng-loading-bar';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from './core/core.module';
+import {environment} from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
 
 
 @NgModule({
-  imports: [CoreModule, PagesModule, BrowserModule, BrowserAnimationsModule,
-    ProfileModule, AppRoutingModule, RegistrationModule,
-    NgLoadingBarModule.forRoot(), HttpClientModule
+  imports: [CoreModule,
+    PagesModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ProfileModule,
+    AppRoutingModule,
+    RegistrationModule,
+    NgLoadingBarModule.forRoot(),
+    HttpClientModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
