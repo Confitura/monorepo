5b5ac1094d56a17016980806e7cb6202ee5b1319
Michal Margiel <Michal.Margiel@gmail.com>
2018-01-28 22:51:30 +0100

    limit access to PageRepository

--- jelatyna-backend/src/main/java/pl/confitura/jelatyna/page/PageRepository.java
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/page/PageRepository.java
@@ -1,8 +1,13 @@
 package pl.confitura.jelatyna.page;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestResource(path = "pages")
-public interface PageRepository extends JpaRepository<Page, String> {
+public interface PageRepository extends Repository<Page, String> {
+    Page findOne(String id);
+
+    @PreAuthorize("@security.isAdmin()")
+    Page save(Page page);
 }
