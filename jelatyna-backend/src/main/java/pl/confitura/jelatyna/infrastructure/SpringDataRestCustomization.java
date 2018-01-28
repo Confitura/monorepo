f23979f81bcf0ba6abf1029cbdbef143191d4b03
Michal Margiel <Michal.Margiel@gmail.com>
2018-01-28 22:47:48 +0100

    expose Page Id

--- jelatyna-backend/src/main/java/pl/confitura/jelatyna/infrastructure/SpringDataRestCustomization.java
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/infrastructure/SpringDataRestCustomization.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
+import pl.confitura.jelatyna.page.Page;
 import pl.confitura.jelatyna.partner.Partner;
 import pl.confitura.jelatyna.presentation.Presentation;
 import pl.confitura.jelatyna.presentation.Tag;
@@ -16,7 +17,14 @@ public class SpringDataRestCustomization extends RepositoryRestConfigurerAdapter
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config
-                .exposeIdsFor(Partner.class, User.class, Tag.class, Presentation.class, Vote.class, Participant.class);
+                .exposeIdsFor(
+                        Partner.class,
+                        User.class,
+                        Tag.class,
+                        Presentation.class,
+                        Vote.class,
+                        Participant.class,
+                        Page.class);
     }
 
 }
