1629add891a52077ba10e27f0d716b0d06fc2a7e
Michal Margiel <Michal.Margiel@gmail.com>
2018-04-03 22:46:41 +0200

    cleanup css for partners

--- confitura-page/mock/partners.js
+++ confitura-page/mock/partners.js
@@ -17,9 +17,11 @@ module.exports = (app) => {
     res.send({
       _embedded: {
         partners:
-          new Array(20)
+          [...new Array(20)
             .fill(1)
-            .map(partner)
+            .map(partner),
+            {...partner(), type: 'platinum'}
+          ]
       }
     });
   });
@@ -29,7 +31,7 @@ module.exports = (app) => {
       id: uuid(),
       name: faker.company.companyName(),
       description: faker.lorem.paragraphs(),
-      type: faker.random.arrayElement(['platinum', 'silver', 'gold', 'brown']),
+      type: faker.random.arrayElement(['silver', 'gold', 'brown']),
       www: faker.internet.url(),
       logo: faker.random.arrayElement(['https://2018.confitura.pl/api/resources/photos/350/bf364499-9ac2-4b5c-a6f3-90fe08698272.png', 'https://touk.pl/img/logo.png']),
       published: true
