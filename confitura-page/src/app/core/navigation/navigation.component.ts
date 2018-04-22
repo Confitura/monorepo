bc194cb9eed52462b24a8b01c2106b0fc1399412
Michal Margiel <Michal.Margiel@gmail.com>
2018-04-22 23:55:40 +0200

    fix menu

--- confitura-page/src/app/core/navigation/navigation.component.ts
+++ confitura-page/src/app/core/navigation/navigation.component.ts
@@ -28,7 +28,7 @@ export class NavigationComponent implements OnInit {
     },
     {label: 'speaker\'s zone', url: '/login', show: () => !this.loggedIn},
     {label: 'logout', action: () => this.logout(), show: () => this.loggedIn},
-    {label: 'Vote 4 Papers', url: '/v4p', show: () => !this.loggedIn, clazz: 'pink'},
+    {label: 'Vote 4 Papers', url: '/v4p', clazz: 'pink'},
   ];
 
 
