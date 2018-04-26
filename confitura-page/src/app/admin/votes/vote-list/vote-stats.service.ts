665b26eb758be19cd36e03b37303c133c14110e5
dwiernicki <D.Wiernicki@polish-airports.com>
2018-04-26 15:05:40 +0200

    display votes on presentation page

--- confitura-page/src/app/admin/votes/vote-list/vote-stats.service.ts
+++ confitura-page/src/app/admin/votes/vote-list/vote-stats.service.ts
@@ -2,13 +2,20 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
 import {VoteStats} from './vote-stats.model';
+import {CurrentUser} from '../../../core/security/current-user.service';
+import 'rxjs/add/observable/empty';
 
 @Injectable()
 export class VoteStatsServiceService {
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient,
+                private currentUser: CurrentUser) {
     }
 
     getAll(): Observable<VoteStats[]> {
-        return this.http.get<VoteStats[]>('/votes/statistics');
+        if (this.currentUser.isAdmin()) {
+            return this.http.get<VoteStats[]>('/votes/statistics');
+        } else {
+            return Observable.empty();
+        }
     }
 }
