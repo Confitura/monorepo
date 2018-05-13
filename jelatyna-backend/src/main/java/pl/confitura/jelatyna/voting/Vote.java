ae9a5b979ad4a750fabdfcfa9d345343bc2b8229
Tomasz TJ Janczewski <tomasz@3pio.pl>
2018-05-13 11:33:40 +0200

    Code reformat for space and tabs.

--- jelatyna-backend/src/main/java/pl/confitura/jelatyna/voting/Vote.java
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/voting/Vote.java
@@ -1,9 +1,11 @@
 package pl.confitura.jelatyna.voting;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.time.LocalDateTime;
 import javax.persistence.*;
 import javax.validation.constraints.*;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.hibernate.annotations.GenericGenerator;
@@ -14,21 +16,21 @@ import pl.confitura.jelatyna.presentation.Presentation;
 @Accessors(chain = true)
 public class Vote {
 
-  @Id
-  @GeneratedValue(generator = "uuid2")
-  @GenericGenerator(name = "uuid2", strategy = "uuid2")
-  @Column(columnDefinition = "varchar(100)")
-  private String id;
-  private String token;
-  @Column(name = "vote_order")
-  private Integer order;
-  @OneToOne
-  private Presentation presentation;
-  @Max(1)
-  @Min(-1)
-  private Integer rate;
-  @JsonIgnore
-  private LocalDateTime voteDate;
-  @JsonIgnore
-  private String client;
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "varchar(100)")
+    private String id;
+    private String token;
+    @Column(name = "vote_order")
+    private Integer order;
+    @OneToOne
+    private Presentation presentation;
+    @Max(1)
+    @Min(-1)
+    private Integer rate;
+    @JsonIgnore
+    private LocalDateTime voteDate;
+    @JsonIgnore
+    private String client;
 }
--- jelatyna-backend/src/main/java/pl/confitura/jelatyna/voting/VoteController.java
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/voting/VoteController.java
@@ -3,10 +3,12 @@ package pl.confitura.jelatyna.voting;
 import static java.util.stream.Collectors.toList;
 
 import com.google.common.collect.Lists;
+
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.*;
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.*;
@@ -22,76 +24,76 @@ import pl.confitura.jelatyna.presentation.*;
 @RepositoryRestController
 public class VoteController {
 
-  private PresentationRepository presentationRepository;
-  private VoteRepository voteRepository;
-  private WebUtils webUtils;
-  private LocalValidatorFactoryBean beanValidator;
+    private PresentationRepository presentationRepository;
+    private VoteRepository voteRepository;
+    private WebUtils webUtils;
+    private LocalValidatorFactoryBean beanValidator;
 
-  @Autowired
-  public VoteController(PresentationRepository presentationRepository, VoteRepository voteRepository,
-      WebUtils webUtils, LocalValidatorFactoryBean beanValidator) {
-    this.presentationRepository = presentationRepository;
-    this.voteRepository = voteRepository;
-    this.webUtils = webUtils;
-    this.beanValidator = beanValidator;
-  }
-
-  @InitBinder
-  protected void initBinder(WebDataBinder binder) {
-    binder.addValidators(beanValidator);
-  }
-
-  @RequestMapping(value = "/votes/start/{token}", method = RequestMethod.POST)
-  @Transactional
-  public ResponseEntity<Resources<?>> start(@PathVariable String token) {
-    Set<Vote> votes = voteRepository.findAllForToken(token);
-    if (votes.isEmpty()) {
-      votes = generateVotes(token);
+    @Autowired
+    public VoteController(PresentationRepository presentationRepository, VoteRepository voteRepository,
+                          WebUtils webUtils, LocalValidatorFactoryBean beanValidator) {
+        this.presentationRepository = presentationRepository;
+        this.voteRepository = voteRepository;
+        this.webUtils = webUtils;
+        this.beanValidator = beanValidator;
     }
-    return ResponseEntity.ok(new Resources<>(votes));
-  }
 
-  private Set<Vote> generateVotes(@PathVariable String token) {
-    List<Presentation> presentations = Lists.newArrayList(this.presentationRepository.findAllForV4p());
-    Collections.shuffle(presentations);
-    List<Vote> votes = IntStream.range(0, presentations.size())
-        .mapToObj(idx -> new Vote()
-            .setClient(webUtils.getClientIp())
-            .setToken(token)
-            .setPresentation(presentations.get(idx))
-            .setOrder(idx))
-        .collect(toList());
-    return this.voteRepository.saveAll(votes);
-  }
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(beanValidator);
+    }
 
-  @PostMapping("/votes")
-  @Transactional
-  public ResponseEntity<Resource<Vote>> save(@RequestBody @Valid Vote vote) {
-    Vote loaded = voteRepository.findById(vote.getId());
-    loaded.setRate(vote.getRate());
-    loaded.setVoteDate(LocalDateTime.now());
-    return ResponseEntity.ok(new Resource<>(loaded));
-  }
+    @RequestMapping(value = "/votes/start/{token}", method = RequestMethod.POST)
+    @Transactional
+    public ResponseEntity<Resources<?>> start(@PathVariable String token) {
+        Set<Vote> votes = voteRepository.findAllForToken(token);
+        if (votes.isEmpty()) {
+            votes = generateVotes(token);
+        }
+        return ResponseEntity.ok(new Resources<>(votes));
+    }
+
+    private Set<Vote> generateVotes(@PathVariable String token) {
+        List<Presentation> presentations = Lists.newArrayList(this.presentationRepository.findAllForV4p());
+        Collections.shuffle(presentations);
+        List<Vote> votes = IntStream.range(0, presentations.size())
+                .mapToObj(idx -> new Vote()
+                        .setClient(webUtils.getClientIp())
+                        .setToken(token)
+                        .setPresentation(presentations.get(idx))
+                        .setOrder(idx))
+                .collect(toList());
+        return this.voteRepository.saveAll(votes);
+    }
+
+    @PostMapping("/votes")
+    @Transactional
+    public ResponseEntity<Resource<Vote>> save(@RequestBody @Valid Vote vote) {
+        Vote loaded = voteRepository.findById(vote.getId());
+        loaded.setRate(vote.getRate());
+        loaded.setVoteDate(LocalDateTime.now());
+        return ResponseEntity.ok(new Resource<>(loaded));
+    }
 
 
-  @PreAuthorize("@security.isAdmin()")
-  @GetMapping("/votes/statistics")
-  @ResponseBody
-  @Transactional
-  public List<PresentationStats> statistics() {
-    Iterable<Vote> allVotes = voteRepository.findAll();
-    Stream<Vote> votesStream = StreamSupport.stream(allVotes.spliterator(), true);
+    @PreAuthorize("@security.isAdmin()")
+    @GetMapping("/votes/statistics")
+    @ResponseBody
+    @Transactional
+    public List<PresentationStats> statistics() {
+        Iterable<Vote> allVotes = voteRepository.findAll();
+        Stream<Vote> votesStream = StreamSupport.stream(allVotes.spliterator(), true);
 
-    Map<String, Optional<PresentationStats>> statsByPresentation = votesStream
-        .map(PresentationStats::new)
-        .collect(Collectors.groupingBy(PresentationStats::getPresentationId,
-            Collectors.reducing(PresentationStats::add)));
+        Map<String, Optional<PresentationStats>> statsByPresentation = votesStream
+                .map(PresentationStats::new)
+                .collect(Collectors.groupingBy(PresentationStats::getPresentationId,
+                        Collectors.reducing(PresentationStats::add)));
 
-    return statsByPresentation.values().stream()
-        .filter(Optional::isPresent)
-        .map(Optional::get)
-        .sorted(Comparator.comparing(PresentationStats::getPositiveVotes))
-        .collect(toList());
+        return statsByPresentation.values().stream()
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .sorted(Comparator.comparing(PresentationStats::getPositiveVotes))
+                .collect(toList());
 
-  }
+    }
 }
