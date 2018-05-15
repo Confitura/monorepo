dbfedd393b42515ebf5d559d4cebde6119365ed5
dwiernicki <D.Wiernicki@polish-airports.com>
2018-05-15 18:34:59 +0200

    add some javadoc to not forget what i am doing

---
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/registration/token/package-info.java
@@ -0,0 +1,19 @@
+/**
+ *  flow
+ *
+ *  # user creates account
+ *  # user fills participant questionnaire
+ *  # user buys ticket
+ *  # system generates token for user
+ *  # system sends token
+ *  # user enters token in apps (after logging in)
+ *  # user with token is marked as 'legal' participant
+ *
+ *  to keep in mind:
+ *  - all volunteers are legal participants
+ *  - all speakers are legal participants
+ *
+ */
+package pl.confitura.jelatyna.registration.token;
+
+
