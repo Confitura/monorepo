da014ea8a304db28311caa7e2c7ff6679b772fd2
dwiernicki <D.Wiernicki@polish-airports.com>
2018-05-18 11:58:56 +0200

    make time slot have hours for start and end

--- jelatyna-backend/src/main/java/pl/confitura/jelatyna/agenda/InlineTimeSlot.java
+++ jelatyna-backend/src/main/java/pl/confitura/jelatyna/agenda/InlineTimeSlot.java
@@ -1,12 +1,19 @@
 package pl.confitura.jelatyna.agenda;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
+
+import java.time.LocalTime;
 
 @Projection(name = "inlineTimeSlot", types = {TimeSlot.class})
 interface InlineTimeSlot {
     String getId();
+
+    @Value("#{target.getLabel()}")
     String getLabel();
     boolean getForAllRooms();
     int getDisplayOrder();
 
+    LocalTime getStart();
+    LocalTime getEnd();
 }
