3aa77ecd6c0d3708a6c922497e100b059d37d8f7
Michal Margiel <Michal.Margiel@gmail.com>
2018-05-24 22:22:38 +0200

    fix linting

--- confitura-page/src/app/core/user/user.service.ts
+++ confitura-page/src/app/core/user/user.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable} from 'rxjs/Observable';
 import {User} from './user.model';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {map} from 'rxjs/operators';
