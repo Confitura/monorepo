a4e8b398e72169ff0ad1a13365299c133945cc92
Michal Margiel <Michal.Margiel@gmail.com>
2019-05-27 21:36:48 +0200

    update styes for tickets info

--- confitura-vue/src/views/RegistrationInfoPage.vue
+++ confitura-vue/src/views/RegistrationInfoPage.vue
@@ -2,7 +2,7 @@
     <div class="tickets">
         <PageHeader title="Tickets"/>
         <Box class="content" color="white">
-            <PageFragment name="registration-info"/>
+            <PageFragment name="registration-info" class="tickets__info"/>
         </Box>
         <TheContact id="contact"/>
     </div>
@@ -21,19 +21,35 @@
 </script>
 
 <style lang="scss">
-.tickets {
-    .table {
-        width: 100%;
-        border-collapse: collapse;
-        td, th {
-            padding: 0.75rem;
-            border-bottom: 1px #dee2e6 solid;
-            box-sizing: border-box;
+    @import "../assets/colors";
 
+    .tickets__info {
+        font-size: 1.5rem;
+        line-height: 2rem;
+
+        a{
+            color: $brand;
+            text-decoration: none;
+
+            &:hover {
+                text-decoration: underline;
+            }
         }
-        tr {
-            box-sizing: border-box;
+
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+
+            td, th {
+                padding: 0.75rem;
+                border-bottom: 1px #dee2e6 solid;
+                box-sizing: border-box;
+
+            }
+
+            tr {
+                box-sizing: border-box;
+            }
         }
     }
-}
 </style>
