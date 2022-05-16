87eaaf6c9f04ed9a2f2e61567fc738816885dd2b
dmnk89 <dmnk.89@gmail.com>
2022-05-16 23:16:45 +0200

    display tags count

--- confitura-vue/src/views/admin/Status.vue
+++ confitura-vue/src/views/admin/Status.vue
@@ -33,12 +33,35 @@
         <span> Build date: {{ buildDate }} </span>
       </div>
     </div>
+
+    <div class="col s12 m4">
+      <div class="card-panel">
+        <span>Tags from submitted presentations:</span>
+        <ol>
+          <li v-for="tag in submittedTags" :key="tag[0]">
+            {{ tag[0] }} - {{ tag[1] }}
+          </li>
+        </ol>
+      </div>
+    </div>
+
+    <div class="col s12 m4">
+      <div class="card-panel">
+        <span>Tags from accepted presentations:</span>
+        <ol>
+          <li v-for="tag in acceptedTags" :key="tag[0]">
+            {{ tag[0] }} - {{ tag[1] }}
+          </li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import dayjs from "dayjs";
+import { Presentation } from "@/types";
 
 const dateFormat = "DD.MM.YYYY HH:mm";
 
@@ -105,6 +128,30 @@ export default class Status extends Vue {
 
   get volunteerCount() {
     return this.$store.getters.volunteerCount;
+  }
+
+  get submittedTags() {
+    return this.countTags(this.$store.state.admin.presentations);
+  }
+
+  get acceptedTags() {
+    let presentations = this.$store.state.admin.presentations.filter(
+      (it: Presentation) => it.status === "accepted"
+    );
+
+    return this.countTags(presentations);
+  }
+
+  countTags(presentations: Presentation[]) {
+    let tagCountMap: Map<string, number> = presentations
+      .flatMap(it => it.tags)
+      .map(it => it.name)
+      .reduce((acc: Map<string, number>, curr) => {
+        let count = acc.get(curr) || 0;
+        acc.set(curr, count + 1);
+        return acc;
+      }, new Map());
+    return Array.from(tagCountMap).sort((l, r) => r[1] - l[1]);
   }
 }
 </script>
