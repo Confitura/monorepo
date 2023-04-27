84fbd23e7f07ac7730239049ee2da4786c763d3d
dominik.wiernicki <dominik.wiernicki@kitopi.com>
2023-04-27 11:06:15 +0200

    open c4p

--- confitura-vue/src/components/HomeMainBanner.vue
+++ confitura-vue/src/components/HomeMainBanner.vue
@@ -9,6 +9,7 @@
 <!--        <TheTimer />-->
         <div class="info__live">
           <div class="info__live-text">CONFITURA 2023</div>
+          <a class="info__live-link" href="https://2023.confitura.pl/c4p" target="_blank" >Call 4 Papers is active!</a>
         </div>
 
         <div class="info__long">
