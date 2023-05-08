6b769e024e8b236f99dfd51b0f04cf6c1814528a
Lukasz Lenart <lukasz.lenart@gmail.com>
2023-05-08 11:51:07 +0200

    Adds space between text and link

--- confitura-vue/src/components/HomeMainBanner.vue
+++ confitura-vue/src/components/HomeMainBanner.vue
@@ -9,8 +9,8 @@
 <!--        <TheTimer />-->
         <div class="info__live">
           <div class="info__live-text">CONFITURA 2023</div>
-          <span class="info__live-link">Call 4 Papers is active!</span>
-          <a class="info__live-link" href="https://2023.confitura.pl/c4p" target="_blank" >register here</a>
+          <span class="info__live-link">Call 4 Papers is active!</span><br/>
+          <a class="info__live-link" href="https://2023.confitura.pl/c4p" target="_blank">register here</a>
         </div>
 
         <div class="info__long">
