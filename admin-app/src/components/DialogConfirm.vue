ac29de93f4c2c5a2161a6da9cce0a863d90a5119
dominikwiernicki <dominik@datasetlinks.com>
2025-03-01 20:58:32 +0100

    use admin template for back-office

---
+++ admin-app/src/components/DialogConfirm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const dialog = ref(false)
+const confirmed = ref(false)
+let resolve: (value: boolean) => void
+const message = ref('')
+watch(dialog, (v) => {
+  if (!v) {
+    resolve(confirmed.value)
+  }
+})
+function open(text: string) {
+  confirmed.value = false
+  dialog.value = true
+  message.value = text
+  return new Promise<boolean>((resolveFn) => {
+    resolve = resolveFn
+  })
+}
+function confirm() {
+  confirmed.value = true
+  dialog.value = false
+}
+function cancel() {
+  confirmed.value = false
+  dialog.value = false
+}
+defineExpose({ open })
+</script>
+
+<template>
+  <v-dialog v-model="dialog" max-width="400px">
+    <v-card style="z-index: -1">
+      <v-card-text class="font-weight-bold d-flex">
+        <v-icon class="mr-2" color="warning">$warning</v-icon>
+        <span>{{ message }}</span>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="primary" @click="cancel">Cancel</v-btn>
+        <v-btn color="primary" @click="confirm">Confirm</v-btn>
+        <v-spacer />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
---
+++ admin-app/src/components/layout/AppBar.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { useToggle, useDark } from '@vueuse/core'
+import router from "@/plugins/router.ts";
+const theme = useTheme()
+const { drawer } = storeToRefs(useAppStore())
+const route = useRoute()
+const breadcrumbs = computed(() => {
+  return route!.matched
+    .slice(1)
+    .filter(
+      (item) =>
+        item.meta && item.meta.title && !(item.meta?.breadcrumb === 'hidden'),
+    )
+    .map((r) => ({
+      title: r.meta.title!,
+      disabled:
+        r.meta?.breadcrumb === 'disabled' || r.path === route.path || false,
+      to: r.path,
+    }))
+})
+const isDark = useDark({
+  onChanged(dark: boolean) {
+    theme.global.name.value = dark ? 'dark' : 'light'
+  },
+})
+const toggleDark = useToggle<true, false | null>(isDark)
+function logout() {
+  localStorage.removeItem('token')
+  router.push("/login")
+}
+</script>
+
+<template>
+  <v-app-bar flat>
+    <v-app-bar-nav-icon @click="drawer = !drawer" />
+    <v-breadcrumbs :items="breadcrumbs" />
+    <v-spacer />
+    <div id="app-bar" />
+    <div>
+      <v-switch
+        :model-value="isDark"
+        color=""
+        hide-details
+        density="compact"
+        inset
+        false-icon="mdi-white-balance-sunny"
+        true-icon="mdi-weather-night"
+        style="opacity: 0.8"
+        @update:model-value="toggleDark"
+      />
+    </div>
+    <v-btn
+      icon
+      @click="logout"
+      size="small"
+      class="ml-2"
+      target="_blank"
+    >
+      <v-icon size="30" icon="mdi-logout" />
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style scoped>
+:deep(.v-breadcrumbs-divider) {
+  opacity: 0.5;
+}
+:deep(.v-switch__thumb .v-icon) {
+  --v-icon-size-multiplier: 1.2 !important;
+}
+</style>
