d41b610557fa43ebd04370e94c0374da550ac2a7
dominikwiernicki <dominik@datasetlinks.com>
2025-05-16 12:46:50 +0200

    styles for markdown generated page

--- admin-app/src/pages/privacy-policy.vue
+++ admin-app/src/pages/privacy-policy.vue
@@ -45,7 +45,7 @@ onMounted(async () => {
     <v-row justify="center">
       <v-col cols="12" md="8" lg="6">
         <v-card class="pa-4">
-          <vue-markdown :source="pageData"  :options="markdownOptions"/>
+          <vue-markdown :source="pageData" :options="markdownOptions"/>
         </v-card>
       </v-col>
     </v-row>
@@ -58,3 +58,49 @@ onMounted(async () => {
     icon="custom:confitura"
   />
 </template>
+<style>
+
+h1, h2, h3, h4, h5, h6 {
+  margin: 1.5em 0 0.5em !important;
+  padding-inline-start: 30px;
+}
+
+p {
+  margin: 1em 0 !important;
+  padding-inline-start: 30px;
+}
+
+blockquote {
+  margin: 1em 0 !important;
+  padding: 0.5em 1em !important;
+  border-left: 4px solid rgba(var(--v-theme-primary), 0.5) !important;
+  background-color: rgba(var(--v-theme-primary), 0.05);
+}
+
+ul, ol {
+  margin: 1em 0 !important;
+  padding-left: 30px !important;
+}
+
+li {
+  padding-inline-start: 10px !important;
+  margin-bottom: 0.5em !important;
+}
+
+ul ul, ol ol, ul ol, ol ul {
+  margin: 0.5em 0 !important;
+}
+
+pre, code {
+  background-color: rgba(var(--v-theme-surface), 0.12);
+  padding: 0.2em 0.4em;
+  border-radius: 4px;
+  font-family: monospace;
+}
+
+pre code {
+  padding: 1em;
+  display: block;
+  overflow-x: auto;
+}
+</style>
