16b830ae880f354fcc6c02534729abd13d244a37
dominikwiernicki <dominik@datasetlinks.com>
2025-07-10 12:42:25 +0200

    add newsletter section and update node engine requirement

--- webpage/components/Contact.vue
+++ webpage/components/Contact.vue
@@ -5,6 +5,14 @@
       <div class="contact__mail">
         <a class="contact__link" href="mailto:confitura@confitura.pl">confitura@confitura.pl</a>
         <div class="contact__socials">
+          <span>
+            newsletter:
+          <a title="newsletter" href="https://mailchi.mp/1811b15cffa4/confitura-newsletter" target="_blank"
+             rel="noopener" class="link"> <i
+              class="fas fa-envelope-square"></i> </a>
+            </span>
+          <span>
+          socials:
           <a title="x" href="https://x.com/confiturapl" target="_blank" rel="noopener" class="link"> <i
               class="fa-brands fa-square-x-twitter"></i> </a>
           <a title="facebook" href="https://www.facebook.com/Confitura" target="_blank" rel="noopener" class="link"> <i
@@ -14,9 +22,7 @@
               class="fab fa-linkedin"></i> </a>
           <a title="youtube" href="https://www.youtube.com/confiturapl" target="_blank" rel="noopener" class="link"> <i
               class="fab fa-youtube-square"></i> </a>
-          <a title="newsletter" href="https://mailchi.mp/1811b15cffa4/confitura-newsletter" target="_blank"
-             rel="noopener" class="link"> <i
-              class="fas fa-envelope-square"></i> </a>
+            </span>
         </div>
         <router-link to="privacy-policy" class="contact__privacy">Privacy policy</router-link>
         <SocialLinks class="mobile"/>
@@ -52,14 +58,16 @@
     }
   }
 
-  .contact__socials .link {
-    padding-right: 0.5rem;
-
-
+  .contact__socials {
     color: #ffffff;
 
-    :hover {
-      color: $brand;
+    .link {
+      padding-right: 0.5rem;
+      color: #ffffff;
+
+      :hover {
+        color: $brand;
+      }
     }
   }
 
