47dc55a0a81232c7262ecd5e2d84896baa5767cb
dominikwiernicki <dominik@datasetlinks.com>
2025-08-10 18:19:39 +0200

    dodanie relacji videło

---
+++ webpage/components/home/main-banner/YtVideo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="video">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/beXgWsLBgUM?si=6BVWntvEUMKCh4W5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+</template>
+<style scoped lang="scss">
+</style>
