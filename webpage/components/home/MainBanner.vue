d5443b2a553ebd3d4f32d2fc13421e7c19be35a3
dominikwiernicki <dominik@datasetlinks.com>
2026-09-25 09:50:48 +0200

    Comment out unused sections in MainBanner.vue for cleanup and future flexibility

--- webpage/components/home/MainBanner.vue
+++ webpage/components/home/MainBanner.vue
@@ -15,18 +15,18 @@
 <!--                  <TheTimer/>-->
                   <p> Warsaw | September 25–26, 2026 </p>
                   <p>ADN Conference Center, Grzybowska 56</p>
-                  <p><NuxtLink to="/calendar" class="banner__calendar-link"><i class="fas fa-calendar-plus"></i> Add to your calendar</NuxtLink></p>
+<!--                  <p><NuxtLink to="/calendar" class="banner__calendar-link"><i class="fas fa-calendar-plus"></i> Add to your calendar</NuxtLink></p>-->
                 </div>
 
 <!--                <div class="info__live">-->
 <!--                  <a class="info__live-text" href="/tickets">Tickets are here!</a>-->
 <!--                </div>-->
-        <div class="info__long">
-          <small>
-            Want to sponsor Confitura? Drop us a line:
-            <a href="mailto:confitura[at]confitura[dot]pl">confitura[at]confitura[dot]pl</a>
-            - let’s team up!
-          </small>
+<!--        <div class="info__long">-->
+<!--          <small>-->
+<!--            Want to sponsor Confitura? Drop us a line:-->
+<!--            <a href="mailto:confitura[at]confitura[dot]pl">confitura[at]confitura[dot]pl</a>-->
+<!--            - let’s team up!-->
+<!--          </small>-->
         </div>
       </div>
       <HomeMainBannerYtVideo/>
