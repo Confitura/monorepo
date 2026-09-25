cc4e7847b0c9e8d48b1f27904270805352ba58e0
dominikwiernicki <dominik@datasetlinks.com>
2026-09-25 09:42:57 +0200

    Hide unused TheTimer component in MainBanner

--- webpage/components/home/MainBanner.vue
+++ webpage/components/home/MainBanner.vue
@@ -12,7 +12,7 @@
 <!--            socials</p>-->
 <!--        </div>-->
                 <div class="info__long">
-                  <TheTimer/>
+<!--                  <TheTimer/>-->
                   <p> Warsaw | September 25–26, 2026 </p>
                   <p>ADN Conference Center, Grzybowska 56</p>
                   <p><NuxtLink to="/calendar" class="banner__calendar-link"><i class="fas fa-calendar-plus"></i> Add to your calendar</NuxtLink></p>
